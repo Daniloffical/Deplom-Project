e2a68812949f3e7bd7f53f435c863372d903c9dc
MEGA Admin <dansus7436@gmail.com>
2023-06-06 20:10:31 +0100

    reforged page profile.html and fixed bugs with images in unauthenticated users

--- FileJet/MainApp/templatetags/base.py
+++ FileJet/MainApp/templatetags/base.py
@@ -1,10 +1,10 @@
 from django import template
 from ProfileApp.models import Profile
-from django.contrib.auth.decorators import login_required
+
 
 register = template.Library()
 
-@login_required(login_url='/example url you want redirect/')
+
 @register.simple_tag
 def give_user(request):
     if request.user.is_authenticated:
--- FileJet/ProfileApp/views.py
+++ FileJet/ProfileApp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url='registration')
 def show_profile(request):
-    return render(request, 'profile.html')
+    context = {}
+    profile = Profile.objects.get(user=request.user)
+    context['profile'] = profile
+    return render(request, 'profile.html', context)
